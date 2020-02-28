e75e1aedd244cabff3bbe923239145a9cab0d413
parvin shafiee <parvin.shafiee@bitoreq.se>
2020-02-28 16:37:20 +0100

    AddTicket RegNo. Generated Automatically

--- TicketManagementSystem/Controllers/TicketsController.cs
+++ TicketManagementSystem/Controllers/TicketsController.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -15,11 +17,13 @@ namespace TicketManagementSystem.Controllers
     [Authorize]
     public class TicketsController : Controller
     {
+        private readonly UserManager<ApplicationUser> userManager;
         private readonly ApplicationDbContext _context;
 
-        public TicketsController(ApplicationDbContext context)
+        public TicketsController(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
         {
             _context = context;
+            this.userManager = userManager;
         }
 
         // GET: Tickets
@@ -104,13 +108,39 @@ namespace TicketManagementSystem.Controllers
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> AddTicket([Bind("Id,RefNo,Title,Problem,CreatedBy,CreatedDate,AssignedTo,HoursSpent,Status,ProjectId,CustomerPriority,RealPriority,DueDate,ClosedDate,LastUpdated,ResponseType,ResponseDesc")] Ticket ticket)
+        public async Task<IActionResult> AddTicket([Bind("Id,Title,Problem,CreatedBy,CreatedDate,AssignedTo,HoursSpent,Status,ProjectId,CustomerPriority,RealPriority,DueDate,ClosedDate,LastUpdated,ResponseType,ResponseDesc")] Ticket ticket)
         {
-            //ticket.RefNo = "BITRQ12345";
-            //ticket.CreatedBy = "Developer1";
+            //Getting LoggedInUser's ComapnyId & then CompanyAbbr & Last RefNo of that company
+            ApplicationUser loggedInUser = await userManager.GetUserAsync(User);
+            var companyAbbr = _context.Companies.Find(loggedInUser.CompanyId).CompanyAbbr;
+            //var companyLastRefNo = _context.Tickets.LastOrDefault(t => t.RefNo.Contains(companyAbbr)).RefNo;
+            var companyLastRefNo = _context.Tickets.Where(t => t.RefNo.Contains(companyAbbr)).ToList().LastOrDefault().RefNo;
+
+            //Increasin that last RefNo by 1 and assigning it to the newly added ticket
+            ticket.RefNo = Regex.Replace(companyLastRefNo, "\\d+",
+                m => (int.Parse(m.Value) + 1).ToString(new string('0', m.Value.Length)));
+
             ticket.Status = Status.Draft;
             ticket.DueDate = DateTime.Now;
 
+
+
+            //var userId = userManager.GetUserAsync(User);
+            //var user = userManager.FindByIdAsync(ticket.CreatedBy);
+
+            //var companyLastRefNo = await _context.Tickets
+            //    .LastOrDefaultAsync(m => m.RefNo == id)
+            //var companyLastRefNo = User.
+            //var companyLastRefNo =  _context.Users
+            //    .Where(u => u.Id == ticket.CreatedBy);
+            //    .Include(a => a.CreatedUser)
+            //    .ThenInclude(m => m.Company)
+            //    .LastOrDefaultAsync(a => a.RefNo.Substring(0,5) == );
+
+            //_context.Tickets.(ticket.CreatedUser.CompanyId)
+            //var newString = Regex.Replace(x, "\\d+",
+            //    m => (int.Parse(m.Value) + 1).ToString(new string('0', m.Value.Length)));
+            //ticket.RefNo
             if (ModelState.IsValid)
             {                
                 _context.Add(ticket);
